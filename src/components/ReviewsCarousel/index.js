41dba0d21e118d70258a8292bf4aac3a351a3103
harshavardhan penumala <penumalaharshavardhan@gmail.com>
2024-12-04 11:56:49 +0530

    Update index.js

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -1,70 +1,83 @@
-.app-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-image: url('https://assets.ccbp.in/frontend/react-js/reviews-bg.png');
-  background-size: cover;
-  height: 100vh;
-}
+import {Component} from 'react'
 
-.heading {
-  color: #ffffff;
-  font-family: 'Roboto';
-  font-size: 40px;
-  font-weight: 500;
-  margin-top: 64px;
-}
+import './index.css'
 
-@media screen and (min-width: 768px) {
-  .heading {
-    font-size: 48px;
-    margin-top: 150px;
+class ReviewsCarousel extends Component {
+  state = {
+    activeReviewIndex: 0,
+  }
+
+  onClickRightArrow = () => {
+    const {activeReviewIndex} = this.state
+    const {reviewsList} = this.props
+
+    if (activeReviewIndex < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex + 1,
+      }))
+    }
+  }
+
+  renderActiveReview = review => {
+    const {imgUrl, username, companyName, description} = review
+
+    return (
+      <div className="review-container">
+        <img src={imgUrl} alt={username} />
+        <p className="username">{username}</p>
+        <p className="company">{companyName}</p>
+        <p className="description">{description}</p>
+      </div>
+    )
+  }
+
+  onClickLeftArrow = () => {
+    const {activeReviewIndex} = this.state
+
+    if (activeReviewIndex > 0) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex - 1,
+      }))
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {activeReviewIndex} = this.state
+    const currentReviewDetails = reviewsList[activeReviewIndex]
+
+    return (
+      <div className="app-container">
+        <h1 className="heading">Reviews</h1>
+        <div className="carousel-container">
+          <button
+            type="button"
+            onClick={this.onClickLeftArrow}
+            data-testid="leftArrow"
+            className="arrow-button"
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+              alt="left arrow"
+            />
+          </button>
+          {this.renderActiveReview(currentReviewDetails)}
+          <button
+            type="button"
+            onClick={this.onClickRightArrow}
+            data-testid="rightArrow"
+            className="arrow-button"
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+              alt="right arrow"
+            />
+          </button>
+        </div>
+      </div>
+    )
   }
 }
 
-.carousel-container {
-  display: flex;
-  align-items: center;
-  width: 85%;
-}
+export default ReviewsCarousel
 
-@media screen and (min-width: 768px) {
-  .carousel-container {
-    width: 45%;
-    min-width: 622px;
-  }
-}
-
-.arrow-button {
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  outline: none;
-}
-
-.review-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 300px;
-}
-
-.username {
-  color: #ffffff;
-  font-family: 'Roboto';
-  font-size: 18px;
-  font-weight: 700;
-}
-
-.company {
-  color: #171f46;
-  font-family: 'Roboto';
-  font-size: 14px;
-}
-
-.description {
-  text-align: center;
-  color: #171f46;
-  font-family: 'Roboto';
-  font-size: 16px;
-}
